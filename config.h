ca473ac4de58dd7df8ba1ea87f3718c9d44e1323
spx01 <42381535+spx01@users.noreply.github.com>
2020-07-03 15:47:28 +0300

    change bindings for keyboard scrolling

--- config.h
+++ config.h
@@ -263,8 +263,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_asciitilde,  keyboard_select,{.i =  0} },
 	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_K,           kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_J,           kscrolldown,    {.i = -1} },
 	{ TERMMOD,              XK_U,           externalpipe,   {.v = openurlcmd} },
 };
 
